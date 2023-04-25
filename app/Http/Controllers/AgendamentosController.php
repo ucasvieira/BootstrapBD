e09372c61f963ce724d009526f6779dfba59aa13
ucasvieira <lucas.souza1012@etec.sp.gov.br>
2023-04-25 10:40:30 -0300

    Banco de Dados

---
+++ app/Http/Controllers/AgendamentosController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Agendamentos;
+
+class AgendamentosController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nome' => 'required|max:255',
+            'telefone' => 'required|max:20',
+            'origem' => 'required|max:255',
+            'contato' => 'required|max:255',
+            'observacao' => 'required|max:255',
+            'acao' => 'required|max:255',
+        ]);
+        $agendamento = new Agendamentos();
+        $agendamento->nome = $request->nome;
+        $agendamento->telefone = $request->telefone;
+        $agendamento->origem = $request->origem;
+        $agendamento->datadecontato = $request->contato;
+        $agendamento->observacao = $request->observacao;
+        $agendamento->acao = $request->acao;
+        $agendamento->save();
+
+        return redirect()->route('index')
+            ->with('Feito', 'Agendado com sucesso!'); }
+    public function show(){
+        $tableagenda = Agendamentos::all();
+        return view('consulta', ['agendamentos' => $tableagenda]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AgendamentosController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::post('/cadastrar', [AgendamentosController::class, 'store'])->name('agendamentos.store');
+
+Route::get('/', function () {
+    return view('index');
+})->name('index');
+
+Route::get('/consulta', [AgendamentosController::class, 'show'])->name('agendamentos.show');
+
